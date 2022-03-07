5f0720adfb05b9be8a2838516ac493d48c8ad214
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-03-07 09:06:07 +0500

    commit

---
+++ main.js
@@ -0,0 +1,245 @@
+
+// 1 masala togri shu sonlarni bolinuvchilarini topish
+// let a = 10
+// for (let i = 2; i < a; i++) {
+//   if (a % i == 0) {
+//     console.log(i)
+//   }
+// }
+
+
+// 2 masala togri
+// let a = 15
+// let sum = 0
+// for (let i = 2; i < a; i++) {
+//   if (a % i == 0) {
+//     sum += i
+//   }
+// }
+// console.log(sum)
+
+
+
+//3 masala mukammal son togri
+// let n = 8
+// sum  = 0
+//  for (let i = 0; i < n ; i++) {
+//   if (n % i == 0) {
+//     sum += i 
+//   }
+// }
+
+// if (sum == n){
+//   console.log("bu mukammal son")
+//  }
+//  else{
+//    console.log("bu mukammal son emas")
+//  }
+
+
+
+
+// 4 masala hat
+// let n = 8
+// sum  = 0
+//  for (let i = 0; i < n ; i++) {
+//   if (n % i == 0) {
+//     sum += i 
+//   }
+// }
+
+// if (sum == n){
+//   console.log("bu mukammal son")
+//  }
+//  else{
+//    console.log("bu mukammal son emas")
+//  }
+
+ 
+
+// 5 masala togri
+// let a = 20
+// for (let i = 0; i<a; i++) {
+//   if (i % 3 == 0) {
+//     console.log(i)
+//   }
+// }
+
+
+
+
+
+// 8 masala
+// let n = 20
+// let a = 3
+// let b = 4
+// let c = 5
+// for (let i = 0; i < n; i++) {
+//   if (c*c == a*a + b*b) {
+//     console.log(i)
+//   }
+// }
+
+
+
+// 9 masala togri 
+// let a = 12
+// let b = 11
+// sum1 = 0
+// sum2 = 0
+// for (let i = 0; i < a; i++){
+//   if (a % i == 0){
+//     sum1 += i
+//   }
+// }
+// for (let j = 0; j < a; j++) {
+//   if (b % j == 0){
+//     sum2 += j
+//   }
+// }
+
+// if (sum1 == sum2) {
+//   console.log("dost son");
+// }
+// else {
+//   console.log("yoooqqqq");
+// }
+
+
+
+
+
+// function s11 () {
+//   let a = 3
+//   let b = 2
+//   let c = 1
+//   let d = 5
+//   let x
+//   let y
+//   if (a > b && a > c && b > c) {
+    
+//   }
+// }
+// s11()
+
+
+
+
+// 12 masala
+// function s12 () {
+//   let a = 1
+//   let b = 2
+//   let c = 3
+//   let a1
+//   let b1
+//   let c1
+//   if (a > b && a > c && b > c) {
+//     a1 = c
+//     c1 = a
+//     console.log(`teskarisiga faqat eng katta son ${a1} eng kichig ${c1}`)
+//   }
+//   else if (c > a && c > b && b > a){
+//     c1 = a
+//     a1 = c
+//     console.log(`teskarisiga faqat eng katta son ${c1} eng kichigg ${a1}`)
+//   }
+// }
+// s12()
+
+
+
+// 13 masala
+// function s13 () {
+//   let a = 1
+//   let b = 2
+//   let c = 3
+//   let a1
+//   let b1
+//   let c1
+//   if (a > b && a > c && b > c) {
+//     c1 = c
+//     a1 = a
+//     console.log(` eng katta son ${a1} eng kichig ${c1}`)
+//   }
+//   else if (c > a && c > b && b > a){
+//     a1 = a
+//     c1 = c
+//     console.log(`eng katta son ${c1} eng kichigg ${a1}`)
+//   }
+// }
+// s13()
+
+
+
+// 14 masala
+// function s14 () {
+//   let a = 1
+//   let b = 2
+//   let c = 3
+//   let a1 = b
+//   let b1 = c
+//   let c1 = a
+
+//   console.log(a1, b1, c1)
+// }
+// s14()
+
+
+// 15 masala
+// function s15 () {
+//   let a = 1
+//   let b = 2
+//   let c = 3
+//   let c1 = b
+//   let b1 = a
+//   let a1 = c
+
+//   console.log(a1, b1, c1)
+// }
+// s15()
+
+
+
+// 16 masala
+// let a = -18787
+// function ishora () {
+//   if (a > 0){
+//     console.log(1)
+//   }
+//   else if (a < 0){
+//     console.log(-1)
+//   }
+//   else if (a = 0){
+//     console.log(0)
+//   }
+// }
+// ishora ()
+
+
+// 17 masala tenglama togri
+// let a = 1
+// let b = 5
+// let c = 0
+// let D
+// function kvadrat () {
+//   D = (b * b) - (4 * a * c)
+//   if (D < 0) {
+//     console.log("tenglama ildizga ega emas");
+//   }
+//   else if (D == 0) {
+//     console.log("1 ta ildizga ega");
+//   }
+//   else if (D > 0) {
+//     console.log(" tenglama 2ta ildizga ega");
+//   }
+// }
+// kvadrat()
+
+
+
+
+
+
+
+
+
